36306c7eacf142bd8da797ddd39695a8f9bdaaf2
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-29 14:59:34 +0300

    add header

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html',
+      template: './src/template.html',
     }),
   ],
   output: {

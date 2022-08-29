3cad70cba3953d8c548c218d367449c11611b31f
binhussen <mhdhsna@gmail.com>
2022-08-29 18:57:15 +0300

    config Url

---
+++ src/modules/config.js
@@ -0,0 +1,6 @@
+/** @format */
+
+export const BASEAPI_URL =
+	'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
+export const INVOLVEMENT_URL =
+	'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/sAxQtNP5nzOV9l3P6DO3';

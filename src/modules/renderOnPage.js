9aba41a63b445f8880d31ac3b1c1a7acdb05ae59
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-29 16:02:14 +0300

    add function to render loaded meal data on page

---
+++ src/modules/renderOnPage.js
@@ -0,0 +1,20 @@
+const renderOnPage = (meal) => {
+  const html = `<div class="card"  data-id = "${meal.idMeal}">
+    <div class="img-container">
+        <img src="${meal.strMealThumb}" alt="">
+    </div>
+    <div class="content">
+      <div class="name">${meal.strMeal}</div>
+      <i class="fa-regular fa-heart like-btn"></i>
+    </div>
+    <span class="likes">5 likes</span>
+    <button class="comment-btn">Comments</button>
+    <button class="reservation-btn">Reservations</button>
+    <div class="comment-pop-up">
+    </div>
+  </div>`;
+  const cardContainer = document.querySelector('.cards-container');
+  cardContainer.innerHTML += html;
+};
+
+export default renderOnPage;

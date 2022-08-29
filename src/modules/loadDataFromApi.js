16900d238ccef4206a5cb97b3ad9b7b026e40836
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-29 17:48:18 +0300

    modify variable scope based on review

--- src/modules/loadDataFromApi.js
+++ src/modules/loadDataFromApi.js
@@ -1,7 +1,7 @@
 import renderOnPage from './renderOnPage.js';
+const baseApiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
 
 const loadDataFromAPI = async () => {
-  const baseApiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
   const response = await fetch(baseApiUrl);
   const { meals } = await response.json();
   // const likes = await getLikes();

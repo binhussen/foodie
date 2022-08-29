a97492ddc7883f5b5bc8af99c83da239671f19fb
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-29 20:18:45 +0300

    add event listner for like btn when clicked

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 /** @format */
 import './styles.css';
 import loadDataFromAPI from './modules/loadDataFromApi.js';
+import updateLikes from './modules/updateLikes.js';
 
 window.onload = () => {
   loadDataFromAPI();
 };
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('.like-btn')) {
+    return;
+  }
+  if (e.target.matches('.like-btn')) {
+    const likeBtns = document.querySelectorAll('.like-btn');
+    likeBtns.forEach((btn, index) => {
+      if (e.target === btn) {
+        updateLikes(index);
+      }
+    });
+  }
+});

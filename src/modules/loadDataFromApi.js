87b21f5db7ce2e7b0fb76c1f57df709ba1700910
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-29 17:58:23 +0300

    add load likes after loading meals from theAPI

--- src/modules/loadDataFromApi.js
+++ src/modules/loadDataFromApi.js
@@ -1,12 +1,14 @@
 import renderOnPage from './renderOnPage.js';
+import getLikes from './getLikes.js';
+
 const baseApiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
 
 const loadDataFromAPI = async () => {
   const response = await fetch(baseApiUrl);
   const { meals } = await response.json();
-  // const likes = await getLikes();
-  meals.forEach((meal) => {
-    renderOnPage(meal);
+  const likes = await getLikes();
+  meals.forEach((meal, index) => {
+    renderOnPage(meal, likes[index]);
   });
 };
 export default loadDataFromAPI;

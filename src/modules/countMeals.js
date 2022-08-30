eff5eff0ce68af4a12d10f0a7a6a0187985db488
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-30 17:24:43 +0300

    modify count meals to get the amount from the cards length

--- src/modules/countMeals.js
+++ src/modules/countMeals.js
@@ -1,10 +1,6 @@
 const countMeals = () => {
   const amount = document.querySelector('.item-amount');
   const items = document.querySelectorAll('.card');
-  let cntr = 0;
-  items.forEach(() => {
-    cntr += 1;
-  });
-  amount.textContent = `(${cntr})`;
+  amount.textContent = `(${items.length})`;
 };
 export default countMeals;

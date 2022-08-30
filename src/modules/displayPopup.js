1433c9ac8aaa89bb83f2c3a331e986b96824a927
binhussen <mhdhsna@gmail.com>
2022-08-30 11:25:38 +0300

    Add Comment Form

--- src/modules/displayPopup.js
+++ src/modules/displayPopup.js
@@ -2,9 +2,9 @@
 import { getMealDetail, getComment } from './operation.js';
 
 const displayPopup = async (mealID) => {
-  const mealDetail = await getMealDetail(mealID);
-  const comments = await getComment(mealID);
-  let html = `<i class="fa-solid fa-x close-btn"></i>
+	const mealDetail = await getMealDetail(mealID);
+	const comments = await getComment(mealID);
+	let html = `<i class="fa-solid fa-x close-btn"></i>
   <div class="item-img">
     <img src="${mealDetail.strMealThumb}" alt="">
   </div>
@@ -17,23 +17,33 @@ const displayPopup = async (mealID) => {
       Comments <span class="comments-amount">(${comments.length ?? 0})</span>
     </div>
     `;
-  if (comments.length > 0) {
-    comments.forEach((comment) => {
-      html += `<div class="comments">
+	if (comments.length > 0) {
+		comments.forEach((comment) => {
+			html += `<div class="comments">
       <span class="comment-date">${comment.creation_date}</span>
       <span class="comment-descritpion">${comment.username}: ${comment.comment}</span>
     </div>`;
-    });
-  }
+		});
+	}
 
-  const popUp = document.querySelector('.comment-pop-up');
-  popUp.innerHTML = html;
-  popUp.classList.add('show-pop-up');
+	html += `<div class="comment-form">
+      <div class="comment-heading">
+        Add a comment
+      </div>
+      <input class="commenter-name" type="text" placeholder="Your name">
+      <textarea class="comment" name="comment" id="" cols="30" rows="10" placeholder="Your insights"></textarea>
+      <button class="add-comment-btn">Comment</button>
+    </div>
+  </div>`;
 
-  const closeBtn = document.querySelector('.close-btn');
-  closeBtn.addEventListener('click', () => {
-    popUp.classList.remove('show-pop-up');
-  });
+	const popUp = document.querySelector('.comment-pop-up');
+	popUp.innerHTML = html;
+	popUp.classList.add('show-pop-up');
+
+	const closeBtn = document.querySelector('.close-btn');
+	closeBtn.addEventListener('click', () => {
+		popUp.classList.remove('show-pop-up');
+	});
 };
 
 export default displayPopup;

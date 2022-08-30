17440ea7450e2e4691f8c74d657cd3e988616c33
binhussen <mhdhsna@gmail.com>
2022-08-30 09:31:06 +0300

    Fix linter Error

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -3,9 +3,9 @@
 import { INVOLVEMENT_URL } from './config.js';
 
 const getLikes = async () => {
-	const responseGet = await fetch(`${INVOLVEMENT_URL}/likes/`);
-	const likes = await responseGet.json();
-	return likes;
+  const responseGet = await fetch(`${INVOLVEMENT_URL}/likes/`);
+  const likes = await responseGet.json();
+  return likes;
 };
 
 export default getLikes;
--- src/modules/loadDataFromApi.js
+++ src/modules/loadDataFromApi.js
@@ -5,11 +5,11 @@ import renderOnPage from './renderOnPage.js';
 import getLikes from './getLikes.js';
 
 const loadDataFromAPI = async () => {
-	const response = await fetch(`${BASEAPI_URL}/filter.php?a=Canadian`);
-	const { meals } = await response.json();
-	const likes = await getLikes();
-	meals.forEach((meal, index) => {
-		renderOnPage(meal, likes[index]);
-	});
+  const response = await fetch(`${BASEAPI_URL}/filter.php?a=Canadian`);
+  const { meals } = await response.json();
+  const likes = await getLikes();
+  meals.forEach((meal, index) => {
+    renderOnPage(meal, likes[index]);
+  });
 };
 export default loadDataFromAPI;

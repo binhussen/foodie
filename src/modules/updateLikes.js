1d0eaf940fb48e57b977020d8201105b2151e482
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-30 22:36:27 +0300

    add like button animation when pressed

--- src/modules/updateLikes.js
+++ src/modules/updateLikes.js
@@ -3,9 +3,15 @@ import getLikes from './getLikes.js';
 
 const updateLikes = async (index) => {
   const likesAmount = document.querySelectorAll('.likes')[index];
+  const heartIcon = document.querySelectorAll('.fa-heart')[index];
+  heartIcon.classList = 'fa-solid fa-heart like-btn';
+  setTimeout(() => {
+    heartIcon.classList = 'fa-regular fa-heart like-btn';
+  }, 120);
   likesAmount.textContent = parseInt(likesAmount.textContent, 10) + 1;
   let likesObj = (await getLikes())[index];
   await createLike(likesObj.item_id);
+
   likesObj = (await getLikes())[index];
 };
 

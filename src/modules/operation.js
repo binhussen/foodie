914d626a2d55a1c56b1309ce920c1cb697450575
binhussen <mhdhsna@gmail.com>
2022-08-30 07:54:04 +0300

    Clear commnets feching

--- src/modules/operation.js
+++ src/modules/operation.js
@@ -2,16 +2,8 @@
 
 import { BASEAPI_URL, INVOLVEMENT_URL } from './config.js';
 
-export const getComment = async (mealID) => {
-  const commentsResponse = await fetch(
-    `${INVOLVEMENT_URL}/comments?item_id=${mealID}`,
-  );
-  const comment = await commentsResponse.json();
-  return comment;
-};
-
 export const getMealDetail = async (mealID) => {
-  const response = await fetch(`${BASEAPI_URL}/lookup.php?i=${mealID}`);
-  const mealDetail = (await response.json()).meals[0];
-  return mealDetail;
+	const response = await fetch(`${BASEAPI_URL}/lookup.php?i=${mealID}`);
+	const mealDetail = (await response.json()).meals[0];
+	return mealDetail;
 };

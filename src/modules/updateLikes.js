93ce040431b3c8be1fe6d2dec7424b0c15173020
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-30 22:06:57 +0300

    modify responsivness of the like button when pressed

--- src/modules/updateLikes.js
+++ src/modules/updateLikes.js
@@ -3,10 +3,10 @@ import getLikes from './getLikes.js';
 
 const updateLikes = async (index) => {
   const likesAmount = document.querySelectorAll('.likes')[index];
+  likesAmount.textContent = parseInt(likesAmount.textContent, 10) + 1;
   let likesObj = (await getLikes())[index];
   await createLike(likesObj.item_id);
   likesObj = (await getLikes())[index];
-  likesAmount.textContent = `${likesObj.likes} likes`;
 };
 
 export default updateLikes;
